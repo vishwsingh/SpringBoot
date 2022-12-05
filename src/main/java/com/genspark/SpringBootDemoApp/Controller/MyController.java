234be9b1c8f11062c52d00ebadfb7482efab7986
vishwsingh <khayekmusic@gmail.com>
2022-12-05 12:39:58 -0500

    Added a requestmapping

--- src/main/java/com/genspark/SpringBootDemoApp/Controller/MyController.java
+++ src/main/java/com/genspark/SpringBootDemoApp/Controller/MyController.java
@@ -1,6 +1,8 @@
 package com.genspark.SpringBootDemoApp.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class MyController {
     @GetMapping("/")
     public String home() {
         return "<HTML><H1>Welcome to the Spring Boot Demo</H1><HTML>";
+    }
+
+    @RequestMapping("page")
+    public String page(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return "<HTML><H1>This is beautiful</H1><HTML>" + name;
     }
 }
